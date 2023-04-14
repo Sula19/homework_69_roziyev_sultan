0ba229226e3c26d66995f663254fc0251df6cb34
Sulatn Roziyev <jjo152065@gmail.com>
2023-04-14 16:46:09 +0600

    Сделал 1 задание

--- core/webapp/views.py
+++ core/webapp/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 def index(request):
     return render(request, 'index.html')
 
 
+@csrf_exempt
 def action(request, *args, **kwargs):
+    data = json.loads(request.body)
+    result = 0
     try:
-        data = {
-            "A": 1234,
-            "B": 4567
-        }
         match request.path:
             case '/add/':
-                result = int(data['A'] + data['B'])
+                result += int(data['A']) + int(data['B'])
             case '/subtract/':
-                result = int(data['A'] - data['B'])
+                result += int(data['A']) - int(data['B'])
             case '/multiply/':
-                result = int(data['A'] * data['B'])
+                result += int(data['A']) * int(data['B'])
             case '/divide/':
-                result = int(data['A'] / data['B'])
+                result += int(data['A']) / int(data['B'])
         answer = {
             'answer': result
         }
         return JsonResponse(answer)
-    except Exception:
-        response = JsonResponse({'error': 'Data is not numbers!'})
+    except ZeroDivisionError:
+        response = JsonResponse({'error': 'Cannot operate on zero'})
+        response.status_code = 400
+        return response
+    except ValueError:
+        response = JsonResponse({'error': 'You entered empty data'})
         response.status_code = 400
         return response
